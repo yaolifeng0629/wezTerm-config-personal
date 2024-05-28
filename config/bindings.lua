23382e91b2e9e242d7b49655e94e097f5fc9da44
Immerse <y15829485647@163.com>
2024-05-28 16:12:58 +0800

    feat: update hot key

--- config/bindings.lua
+++ config/bindings.lua
@@ -247,7 +247,7 @@ local mouse_bindings = {
 
 return {
     disable_default_key_bindings = true,
-    disable_default_mouse_bindings = true,
+    disable_default_mouse_bindings = false,
     leader = { key = 'Space', mods = mod.SUPER_REV },
     keys = keys,
     key_tables = key_tables,
