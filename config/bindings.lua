d622ef4334a5f31a51badc391b93f9ddf4a58872
Immerse <y15829485647@163.com>
2024-05-30 14:55:34 +0800

    feat: disable mouse behavories

--- config/bindings.lua
+++ config/bindings.lua
@@ -195,54 +195,54 @@ local mouse_bindings = {
         action = act.OpenLinkAtMouseCursor,
     },
     -- Move mouse will only select text and not copy text to clipboard
-    {
-        event = { Down = { streak = 1, button = "Left" } },
-        mods = "NONE",
-        action = act.SelectTextAtMouseCursor("Cell"),
-    },
-    {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "NONE",
-        action = act.ExtendSelectionToMouseCursor("Cell"),
-    },
-    {
-        event = { Drag = { streak = 1, button = "Left" } },
-        mods = "NONE",
-        action = act.ExtendSelectionToMouseCursor("Cell"),
-    },
-    -- Triple Left click will select a line
-    {
-        event = { Down = { streak = 3, button = "Left" } },
-        mods = "NONE",
-        action = act.SelectTextAtMouseCursor("Line"),
-    },
-    {
-        event = { Up = { streak = 3, button = "Left" } },
-        mods = "NONE",
-        action = act.SelectTextAtMouseCursor("Line"),
-    },
-    -- Double Left click will select a word
-    {
-        event = { Down = { streak = 2, button = "Left" } },
-        mods = "NONE",
-        action = act.SelectTextAtMouseCursor("Word"),
-    },
-    {
-        event = { Up = { streak = 2, button = "Left" } },
-        mods = "NONE",
-        action = act.SelectTextAtMouseCursor("Word"),
-    },
-    -- Turn on the mouse wheel to scroll the screen
-    {
-        event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-        mods = "NONE",
-        action = act.ScrollByCurrentEventWheelDelta,
-    },
-    {
-        event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-        mods = "NONE",
-        action = act.ScrollByCurrentEventWheelDelta,
-    },
+    -- {
+    --     event = { Down = { streak = 1, button = "Left" } },
+    --     mods = "NONE",
+    --     action = act.SelectTextAtMouseCursor("Cell"),
+    -- },
+    -- {
+    --     event = { Up = { streak = 1, button = "Left" } },
+    --     mods = "NONE",
+    --     action = act.ExtendSelectionToMouseCursor("Cell"),
+    -- },
+    -- {
+    --     event = { Drag = { streak = 1, button = "Left" } },
+    --     mods = "NONE",
+    --     action = act.ExtendSelectionToMouseCursor("Cell"),
+    -- },
+    -- -- Triple Left click will select a line
+    -- {
+    --     event = { Down = { streak = 3, button = "Left" } },
+    --     mods = "NONE",
+    --     action = act.SelectTextAtMouseCursor("Line"),
+    -- },
+    -- {
+    --     event = { Up = { streak = 3, button = "Left" } },
+    --     mods = "NONE",
+    --     action = act.SelectTextAtMouseCursor("Line"),
+    -- },
+    -- -- Double Left click will select a word
+    -- {
+    --     event = { Down = { streak = 2, button = "Left" } },
+    --     mods = "NONE",
+    --     action = act.SelectTextAtMouseCursor("Word"),
+    -- },
+    -- {
+    --     event = { Up = { streak = 2, button = "Left" } },
+    --     mods = "NONE",
+    --     action = act.SelectTextAtMouseCursor("Word"),
+    -- },
+    -- -- Turn on the mouse wheel to scroll the screen
+    -- {
+    --     event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    --     mods = "NONE",
+    --     action = act.ScrollByCurrentEventWheelDelta,
+    -- },
+    -- {
+    --     event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    --     mods = "NONE",
+    --     action = act.ScrollByCurrentEventWheelDelta,
+    -- },
 }
 
 return {
