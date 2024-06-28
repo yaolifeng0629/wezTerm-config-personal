bb7312ab4524cc321035dba1ac4e5ab0d52387c9
Immerse <y15829485647@163.com>
2024-06-29 07:23:12 +0800

    fix: fixed the stuck problem

--- config/appearance.lua
+++ config/appearance.lua
@@ -5,12 +5,12 @@ local colors = require('colors.custom')
 return {
    -- automatically_reload_config = true,
    -- animation_fps = 60,
-   max_fps = 60,
+   -- max_fps = 60,
    exit_behavior = "CloseOnCleanExit",
    -- status_update_interval = 1000,
-   front_end = 'WebGpu',
+   -- front_end = 'WebGpu',
    -- webgpu_power_preference = 'HighPerformance',
-   webgpu_preferred_adapter = gpu_adapters:pick_best(),
+   -- webgpu_preferred_adapter = gpu_adapters:pick_best(),
 
    -- color scheme
     -- colors = colors,
